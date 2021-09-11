a08ee09b649f7f68dd56206f51e1ad7a6e6220db
sypwex <sypwex@gmail.com>
2021-09-12 00:23:11 +0300

    chore[ci]

--- crossbuild.sh
+++ crossbuild.sh
@@ -7,7 +7,7 @@ echo "============================= build base docker image"
 docker build -t vvvvvv-crossbuild -f crossbuild.Dockerfile .
 
 # echo "============================= build"
-docker run --rm -it -v `pwd`:/workdir vvvvvv-crossbuild bash -c "./bin/cargobuild.sh && ./bin/copy.sh"
+docker run --rm -v `pwd`:/workdir vvvvvv-crossbuild bash -c "./bin/cargobuild.sh && ./bin/copy.sh"
 
 # TODO: include version in archive name somehow
 zip -r vvvvvv.zip $target_dir
