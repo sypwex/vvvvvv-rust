81900e518fed3ce9971cf999b1ad0759b1c2b1b1
sypwex <sypwex@gmail.com>
2021-09-12 11:09:24 +0000

    WIP CI

--- bin/copy.sh
+++ bin/copy.sh
@@ -3,9 +3,9 @@
 target_dir=./target/release
 
 echo "============================= copy binaries"
-cp -v target/x86_64-unknown-linux-gnu/debug/vvvvvv_on_rust $target_dir/linux/
-cp -v target/x86_64-apple-darwin/debug/vvvvvv_on_rust $target_dir/mac/
-cp -v target/x86_64-pc-windows-gnu/debug/vvvvvv_on_rust.exe $target_dir/win64/
+cp -v target/x86_64-unknown-linux-gnu/debug/vvvvvv-rust $target_dir/linux/
+cp -v target/x86_64-apple-darwin/debug/vvvvvv-rust $target_dir/mac/
+cp -v target/x86_64-pc-windows-gnu/debug/vvvvvv-rust.exe $target_dir/win64/
 
 echo "============================= copy dependencies"
 cp -v /usr/local/lib/x86_64-unknown-linux-gnu/{libphysfs,libSDL2,libSDL2_mixer}.so $target_dir/linux/
